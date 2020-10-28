7873d41313abf552637f3e329fda6d4e7c068474
Leonel Estudillo Gonzalez <leonel.estudillo@ilm.com.mx>
2020-10-27 21:27:03 -0700

    DIA MARTES

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,12 @@ import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './app.routes';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { KitsComponent } from './kits/kits.component';
+import { MaterialDesignModule } from './material-design/material-design.module'
 
 //Kits
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import { HttpClientModule} from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialDesignModule
   ],
   providers: [],
   bootstrap: [AppComponent]
