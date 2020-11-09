171b5b623791a5bb86022e6060134529e0972e49
ilm-cvargas <christian.rey@ilm.com.mx>
2020-11-09 05:12:34 -0800

    Auto-generated commit

--- src/app/shared/juego.ts
+++
@@ -1,9 +0,0 @@
-//modelo de datos
-export interface Juego {
-    titulo: string
-    subtitulo: string
-    descripcion: string
-    img: string  
-    //declarar un atributo con sufijo ? significa que es un atributo opcional, que algunos objetos lo pueden tener si quieren.
-    img2? :string
-}
